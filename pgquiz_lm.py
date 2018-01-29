4b9673f52efc184c5b0dc4321e0206ee04438eae
lylam09 <lmiller@gcds.net>
2018-01-29 10:54:35 -0500

    Add files via upload

---
+++ pgquiz_lm.py
@@ -0,0 +1,124 @@
+import pyautogui as pg
+import time
+
+points = 0
+
+# Question
+answer = pg.prompt(
+"""
+Which would you rather do?
+
+a) Eat sandwiches and play video games
+b) Cook and clean 
+c) Read fashion magazines 
+
+
+"""
+    )
+
+# Give points
+if answer == "a":
+    points += 1
+elif answer == "b":
+    points += 2
+elif answer == "c":
+    points += 3 
+
+# Question
+answer = pg.prompt(
+"""
+Who would you rather date?
+
+a) No one, commitment is for losers
+b) My former best friend
+c) A smart guy who I've known forever 
+
+"""
+    )
+
+# Give points
+if answer == "a":
+    points += 1
+elif answer == "b":
+    points += 2
+elif answer == "c":
+    points += 3
+
+# Question
+answer = pg.prompt(
+"""
+What would you rather eat for dinner?
+
+a) meatball sandiwch 
+b) something veggie
+c) something posh 
+
+
+"""
+    )
+
+# Give points
+if answer == "a":
+    points += 1
+elif answer == "b":
+    points += 2
+elif answer == "c":
+    points += 3
+
+# Question
+answer = pg.prompt(
+"""
+What is your job?
+
+a) An actor, you may know me from the famous soap opera, Days of Our Lives 
+b) Chef
+c) I used to work at a coffee shop but now I work in fashion 
+
+
+"""
+    )
+
+# Give points
+if answer == "a":
+    points += 1
+elif answer == "b":
+    points += 2
+elif answer == "c":
+    points += 3
+
+# Question
+answer = pg.prompt(
+"""
+What is your job?
+
+a) An actor, you may know me from the famous soap opera, Days of Our Lives 
+b) Chef
+c) I used to work at a coffee shop but now I work in fashion 
+
+
+"""
+    )
+
+# Give points
+if answer == "a":
+    points += 1
+elif answer == "b":
+    points += 2
+elif answer == "c":
+    points += 3
+
+# END OF SURVEY
+
+pg.alert("You are ...")
+
+# Joey
+if points < 4:
+    pg.alert("Joey Tribbiani")
+# Monica
+if points >= 4 and points < 7:
+    pg.alert("Monica Geller")
+# Rachel 
+if points >= 7 and points < 10:
+    pg.alert("Rachel Green")
+
+
